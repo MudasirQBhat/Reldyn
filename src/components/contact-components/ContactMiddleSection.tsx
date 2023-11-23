49b221c0169f8f3bcd11a3e112ec2162a73c46bc
MuddyonGit <mudasirmailg@gmail.com>
2023-11-23 10:30:35 +0530

    made changes in contact page

--- src/components/contact-components/ContactMiddleSection.tsx
+++ src/components/contact-components/ContactMiddleSection.tsx
@@ -6,21 +6,21 @@ const ContactMiddleSection = () => {
   return (
     <>
       <div className="bg-primary-background" data-aos="fade-right">
-        <div className="grid grid-cols-8 lg:grid-cols-12 w-full  px-6 lg:px-20 py-20 pb-80">
-          <div className="w-full  col-span-8 lg:col-span-8 relative lg-bottom-4 lg:-right-4 z-10 lg:-bottom-0">
+        <div className="grid grid-cols-8 lg:grid-cols-8  w-full  px-6 lg:px-20 py-20 pb-80">
+          <div className="w-full col-span-8  xl:col-span-4 relative lg-bottom-4 lg:-right-0 z-10 lg:-bottom-0">
             <ContactForm />
           </div>
-          <div className=" w-full  col-span-8 lg:col-span-4 mb-28 md:mb-0">
+          <div className=" w-full   col-span-8  xl:col-span-4 mb-28 md:mb-0">
             <div className="w-full relative">
               <img
                 src={contactFormImage1}
                 alt="Malaysia Contact Image"
-                className="lg:rounded-tr-3xl w-full"
+                className="lg:rounded-t-3xl w-full md:h-full"
               />
               <h1 className=" font-roboto text-primary-color font-font-700 text-1xl lg:text-2xl absolute top-20 left-4 lg:top-36 lg:left-12">
                 Malaysia
               </h1>
-              <p className="font-roboto text-sm lg:text-base absolute  top-28 left-4 w-72 lg:w-72 lg:top-48 lg:left-12 text-color-natural-black">
+              <p className="font-roboto text-sm lg:text-base absolute  top-28 left-4 w-72 lg:w-72 lg:top-44 lg:left-12 text-color-natural-black">
                 Suite Prague Centrepoint South, Penthouse Level 27, Lingkaran
                 Syed Putra, Mid Valley City, 58000 Kuala Lumpur
               </p>
@@ -29,7 +29,7 @@ const ContactMiddleSection = () => {
               <img
                 src={contactFormImage2}
                 alt="Malaysia Contact Image"
-                className="rounded-b-3xl lg:rounded-br-3xl lg:rounded-bl-none w-full"
+                className="rounded-b-3xl md:rounded-b-3xl lg:rounded-bl-3xl w-full"
               />
               <h1 className=" font-roboto text-primary-color font-font-700 text-1xl lg:text-2xl absolute top-20 left-4 lg:top-36 lg:left-12">
                 India
